85fea032980c4871b3800e7a0fcbbaa2d3cb6049
greenmoon55 <greenmoon55@gmail.com>
2013-11-27 13:33:09 +0800

    rearrange files into folders

--- ocr.py
+++
@@ -1,9 +0,0 @@
-import collections
-f = open('ocr.in', 'r')
-dict = collections.defaultdict(int)
-for line in f:
-    for char in line:
-        dict[char] = dict[char] + 1
-        if char >= 'a' and char <= 'z': print char,
-for key in sorted(dict.keys()):
-    print key, dict[key]
